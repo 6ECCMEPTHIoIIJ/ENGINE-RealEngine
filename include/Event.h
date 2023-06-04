17a5b6ff3fc7a182bbec62567062927e1ee55ec6
6ECCMEPTHIoIIJ <shamraev.alexandr@gmail.com>
2023-06-04 13:14:24 +0300

    /Event system/ + class Event : Object /// +AddListener(); +RemoveListener(); +Invoke(); /// TODO: add operator+=(), operator-=(), operator()(); /// TODO: add multi-thread guard for Event and Handler

--- include/Event.h
+++ include/Event.h
@@ -1,13 +1,15 @@
 #if !defined REAL_ENGINE_EVENT_H_
 #define REAL_ENGINE_EVENT_H_
 
-#include <set>
+#include <unordered_set>
+#include <atomic>
 
 #include "Object.h"
 #include "Handler.h"
 
 namespace re {
 
+template<typename ...Args>
 class Event : public Object {
 // Friend definitions =======
 
@@ -18,6 +20,12 @@ class Event : public Object {
 
  private:
 
+  struct Request {
+    Handler<Args...> *handler;
+
+    void (Event::*operation)(Handler<Args...> *);
+  };
+
 // Fields ===================
  public:
 
@@ -26,10 +34,16 @@ class Event : public Object {
  protected:
 
  private:
+  std::unordered_set<Handler<Args...>> handlers_;
+
+  std::vector<Request> requests_;
+
+  std::atomic<uint16_t> busy_ = 0;
 
 // Methods ==================
  public:
 // Constructors -------------
+
   Event() = default;
 
   Event(const Event &other) = delete;
@@ -45,6 +59,39 @@ class Event : public Object {
 // Setters ------------------
 
 // Other --------------------
+
+  auto AddListener(Handler<Args...> *handler) -> void {
+    if (busy_ != 0) {
+      requests_.push(handler, &Event::AddListener);
+    } else {
+      handlers_.insert(handler);
+    }
+  }
+
+  auto RemoveListener(Handler<Args...> *handler) -> void {
+    if (busy_ != 0) {
+      requests_.push(handler, &Event::RemoveListener);
+    } else {
+      handlers_.erase(handler);
+    }
+  }
+
+  auto Invoke(Args... args) -> void {
+    ++busy_;
+    for (auto &handler : handlers_) {
+      handler->Invoke();
+    }
+    --busy_;
+
+    if (requests_.empty()) {
+      return;
+    }
+
+    for (auto &request : requests_) {
+      request.operation(request.handler);
+    }
+    requests_.clear();
+  }
 
 // Operators ----------------
 
